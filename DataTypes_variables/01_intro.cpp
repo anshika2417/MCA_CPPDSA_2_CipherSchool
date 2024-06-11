7a54b5b48f8aa9d27fefb9ca6143d6c40a9de2b5
anshika2417 <anshikakumari571@gmail.com>
2024-06-11 14:40:31 +0530

    testing

---
+++ DataTypes_variables/01_intro.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b;
+    int c = a+b;
+    cout<<c<<endl;
+    float f=5.5;
+    cout<<f<<endl;
+    double d= 5.55424;
+    cout<<d<<endl;
+    char ch = '1';
+    char g = '2';
+    cout<<ch+g<<endl;
+}
