3a417a74504d2378e09191fe394ba504931b695b
anshika2417 <anshikakumari571@gmail.com>
2024-06-20 14:00:55 +0530

    vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -8,6 +8,19 @@ int main()
 {
     vector<int> vector1 {1,2,3,4};
     
+    //add
+    vector1.push_back(10);
+
+    //updatation
+    //vector1.at(2)=25;
+
+    //access
+    //cout<<vector1[10]<<endl; //while this gives garbage value
+   // cout<<vector1.at(10)<<endl; //this gives out of bound array error
+
+   //deletion
+   vector1.pop_back();
+   
     for(int item:vector1){
         cout<<item<<" ";
     }
