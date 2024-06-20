39251b4955d4a994d822133f4148a18c20b48c07
anshika2417 <anshikakumari571@gmail.com>
2024-06-20 15:27:35 +0530

    alternate

--- DSA_Cipher_Schools/alternate_reverse.cpp
+++ DSA_Cipher_Schools/alternate_reverse.cpp
@@ -1,12 +1,4 @@
-
 //swap alternate elements of array
-//pair sum
-//triplet sum
-//sort 0's and 1's eg: 1010 after sorting: 0011
-//find duplicate elements and find how many numbers are duplicate
-//find unique elements
-//find intersection elements
-//No of occurence of an elements in an array is unique or not
 
 #include<iostream>
 using namespace std;
