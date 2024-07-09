c908386d3ee0d52745adb59c1b21e00a4d36f03f
anshika2417 <anshikakumari571@gmail.com>
2024-07-09 14:58:08 +0530

    Sum of Nodes

---
+++ Tree/SumOfNodes.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d)
+    {
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+int sumOfNodes(Node* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+
+    return sumOfNodes(root->left) + sumOfNodes(root->right) +root->data;
+}
+int main()
+{
+    Node* root=new Node(1);
+    root->left= new Node(2);
+    root->right= new Node(3);
+    root->left->left= new Node(4);
+    root->left->right = new Node(5);
+
+    cout<<sumOfNodes(root);
+}
